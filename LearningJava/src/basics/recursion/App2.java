ecbb0b4adc62ca464202ee895987f136ff249f30
rahuldhillo <anemailformac@gmail.com>
2019-05-02 16:32:59 +0530

    recursin project commited.

--- LearningJava/src/basics/recursion/App2.java
+++ LearningJava/src/basics/recursion/App2.java
@@ -1,4 +1,22 @@
 package basics.recursion;
 
 public class App2 {
+
+    public static void main(String[] args) {
+        calculate(4);
+    }
+
+    private static void calculate(int value){
+
+        System.out.println(value);
+        if (value == 1){
+            return;
+        }
+
+        calculate(value -1); // recursive call
+
+        System.out.println(value + " I am after the recursion"); //Explain this
+    }
 }
+
+
