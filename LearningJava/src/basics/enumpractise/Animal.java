4abf684b90836deba70a905eb0afa74fa798e56e
rahuldhillo <anemailformac@gmail.com>
2019-05-02 13:53:53 +0530

    new changes commited

---
+++ LearningJava/src/basics/enumpractise/Animal.java
@@ -0,0 +1,18 @@
+package basics.enumpractise;
+
+public enum Animal {
+    DOG("Shero"),CAT("Billi"),MOUSE("Jerry");
+
+    private String name;
+    Animal (String name){
+        this.name = name;
+    }
+
+    public String getName (){
+        return name;
+    }
+
+    public String toString(){
+        return "This animal is called: " + name;
+    }
+}
